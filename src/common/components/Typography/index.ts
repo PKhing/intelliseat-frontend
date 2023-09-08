5a2c5ef5a3db29b86dc9065e28d40a9535cce0b1
Pimrak Apiratchatanont <apimrak@gmail.com>
2023-09-08 13:38:35 +0700

    feat: add stitches

---
+++ src/common/components/Typography/index.ts
@@ -0,0 +1,34 @@
+import {styled} from "@stitches/react";
+
+export const Typography = styled("div", {
+  variants: {
+    variant: {
+      paragraph2: {
+        fontFamily: "$roboto",
+        fontSize: "14px",
+      },
+      paragraph1: {
+        fontFamily: "$roboto",
+        fontSize: "16px",
+      },
+      header: {
+        fontFamily: "$roboto",
+        fontSize: "16px",
+        fontWeight: "bold",
+      },
+      title: {
+        fontFamily: "$roboto",
+        fontSize: "20px",
+        fontWeight: "bold",
+      },
+      caption: {
+        fontFamily: "$roboto",
+        fontSize: "12px",
+      },
+    },
+  },
+
+  defaultVariants: {
+    variant: "paragraph1",
+  },
+});
---
+++ src/config/stitches.ts
@@ -0,0 +1,33 @@
+import * as Stitches from "@stitches/react";
+
+export const {styled, keyframes, getCssText, config} = Stitches.createStitches({
+  theme: {
+    colors: {
+      primaryLight: "#BCEDED",
+      primary: "#2BBABA",
+      primaryDark: "#006868",
+      black: "#18023C",
+      gray: "#7E7788",
+    },
+    fonts: {
+      roboto: "'Roboto', sans-serif",
+      inter: "'Inter', sans-serif",
+    },
+  },
+
+  media: {
+    xs: "(max-width: 360px)",
+    sm: "(max-width: 496px)",
+    md: "(max-width: 768px)",
+    lg: "(max-width: 960px)",
+    xl: "(max-width: 1280px)",
+  },
+});
+
+export type CSS = Stitches.CSS<typeof config>;
+
+Stitches.globalCss({
+  "@import": [
+    "url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Roboto:wght@400;500;700&display=swap')",
+  ],
+})();
